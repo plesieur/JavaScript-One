f99930242c982b1d7400a2efa1a4edc1adcba1e3
Paul <71508910+plesieur@users.noreply.github.com>
2024-05-20 07:22:41 -0400

    Add files via upload
    Original

---
+++ Button.js
@@ -0,0 +1,19 @@
+function cat()
+{
+	document.getElementById("animal").innerHTML="cat";
+}
+
+function green()
+{
+	document.getElementById("color").style.color="green";
+}
+
+function dog()
+{
+	document.getElementById("animal").innerHTML="dog";
+}
+
+function red()
+{
+	document.getElementById("color").style.color="red";
+}
